d96a9986a7c26e602d1fcacbae8047373bff1f73
Adam Dera <55030723+ADera1220@users.noreply.github.com>
2020-03-03 13:13:57 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,97 @@
+const menu = [
+    {
+        title: 'menu 1',
+        soup: {
+            name: 'Minestrone',
+            price: '2.95'
+        },
+        salad: {
+            name: 'Greek Salad',
+            price: '4.75',
+        },
+        LighterFare: {
+            name: 'Vegetable Biriyani',
+            price: '4.95'
+        },
+        entree: {
+            name: 'Breaded Chicken on a Bun',
+            price: '5.95'
+        }
+    },
+    {
+        title: 'menu 2',
+        soup: {
+            name: 'Red Lentil Dal',
+            price: '2.95'
+        },
+        salad: {
+            name: 'Julienne Salad',
+            price: '4.75',
+        },
+        LighterFare: {
+            name: 'Madras Chicken Salad Wrap',
+            price: '4.95'
+        },
+        entree: {
+            name: 'Fish and Chips',
+            price: '5.95'
+        }
+    },
+    {
+        title: 'menu 3',
+        soup: {
+            name: 'Spicy Squash and Pumpkin',
+            price: '2.95'
+        },
+        salad: {
+            name: 'Mediterranean Salad',
+            price: '4.75',
+        },
+        LighterFare: {
+            name: 'Ham & Cheese Panini',
+            price: '4.95'
+        },
+        entree: {
+            name: 'Butter Chicken with Rice Pilaf',
+            price: '5.95'
+        }
+    },
+    {
+        title: 'menu 4',
+        soup: {
+            name: 'Clam Chowder',
+            price: '2.95'
+        },
+        salad: {
+            name: 'Israeli Couscous & Feta Salad',
+            price: '4.75',
+        },
+        LighterFare: {
+            name: 'Pulled Pork on a Bun',
+            price: '4.95'
+        },
+        entree: {
+            name: 'Matar Paneer & Basmati Rice',
+            price: '5.95'
+        }
+    },
+    {
+        title: 'menu 5',
+        soup: {
+            name: 'Tomato',
+            price: '2.95'
+        },
+        salad: {
+            name: 'Caeser Salad',
+            price: '4.75',
+        },
+        LighterFare: {
+            name: 'Burger and Fries',
+            price: '4.95'
+        },
+        entree: {
+            name: 'Beef and Broccoli with Fried Rice',
+            price: '5.95'
+        }
+    }
+]
